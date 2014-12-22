42fccff9ed063a5e7fbf7a2e9d5c1e8d95a6c78a
Yuriy <freekvant@gmail.com>
2014-12-22 16:44:37 +0200

    Copy Function

--- DS2_Backup_Tool/MainForm.cs
+++ DS2_Backup_Tool/MainForm.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Windows.Forms;
+    using System.IO;
 
     public partial class MainForm : Form
     {
@@ -31,6 +32,18 @@
         {
             Show();
             WindowState = FormWindowState.Normal;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var save_path = @"C:\temp";
+            string[] files = Directory.GetFiles(save_path);
+            listBox1.Items.Clear();
+            var fileName = Path.Combine((Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)),"Test","Temp","file1.txt");
+            //MessageBox.Show(fileName);
+            File.Copy(fileName, @"C:\temp\copied3.txt" + files.Length);
+            foreach (string s in Directory.GetFiles(save_path))
+                listBox1.Items.Add(Path.GetFileName(s)+"     "+File.GetLastWriteTime(s));
         }
     }
 }
