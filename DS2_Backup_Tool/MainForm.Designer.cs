9ee08a2add2d9b9f9fed42e4089e70662e1d8f35
Yuriy <freekvant@gmail.com>
2014-12-23 19:47:35 +0200

    progress

--- DS2_Backup_Tool/MainForm.Designer.cs
+++ DS2_Backup_Tool/MainForm.Designer.cs
@@ -35,114 +35,53 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(91, 318);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(229, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "%appdata%";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 321);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Save Path";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 355);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Backup Path";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(91, 352);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(229, 20);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "C:\\temp\\";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(352, 316);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(352, 349);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Browse";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Items.AddRange(new object[] {
-            "save1",
-            "save2"});
+            this.listBox1.ItemHeight = 18;
             this.listBox1.Location = new System.Drawing.Point(12, 53);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(308, 244);
+            this.listBox1.Size = new System.Drawing.Size(308, 238);
             this.listBox1.TabIndex = 5;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(352, 127);
+            this.button3.Location = new System.Drawing.Point(326, 82);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "Load F8";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.loadButtonClick);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(352, 172);
+            this.button4.Location = new System.Drawing.Point(326, 123);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 7;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.DeleteButtonClick);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(352, 86);
+            this.button5.Location = new System.Drawing.Point(326, 53);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 8;
             this.button5.Text = "Backup F5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.backupButtonClick);
             // 
             // notifyIcon1
             // 
@@ -151,23 +90,29 @@
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "List of saves";
+            // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(450, 390);
+            this.ClientSize = new System.Drawing.Size(428, 316);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "DS2 Backup Tool";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,18 +120,12 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private Label label1;
-        private Label label2;
-        private TextBox textBox2;
-        private Button button1;
-        private Button button2;
         private ListBox listBox1;
         private Button button3;
         private Button button4;
         private Button button5;
-        private FolderBrowserDialog folderBrowserDialog1;
         private NotifyIcon notifyIcon1;
+        private Label label1;
     }
 }
 
--- DS2_Backup_Tool/MainForm.cs
+++ DS2_Backup_Tool/MainForm.cs
@@ -3,30 +3,33 @@
     using System;
     using System.Windows.Forms;
     using System.IO;
+    using System.Collections.Generic;
 
     public partial class MainForm : Form
     {
+        string save_path = @"C:\temp";
+        Dictionary<int, string> dic;
         public MainForm()
         {
             InitializeComponent();
             ShowInTaskbar = false;
+            dic = new Dictionary<int, string>();
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void showFiles()
         {
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            listBox1.Items.Clear();
+            
+            foreach (string s in Directory.GetFiles(save_path))
             {
-                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                listBox1.Items.Add(Path.GetFileName(s) + "     " + File.GetLastWriteTime(s));
+                if (listBox1.Items.Count - 1 >= 0)
+                    dic.Add(listBox1.Items.Count-1, s);
             }
+            
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-            {
-                textBox2.Text = folderBrowserDialog1.SelectedPath;
-            }
-        }
 
         private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
         {
@@ -34,16 +37,34 @@
             WindowState = FormWindowState.Normal;
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void loadButtonClick(object sender, EventArgs e)
         {
-            var save_path = @"C:\temp";
+            MessageBox.Show(dic[listBox1.SelectedIndex]);
+        }
+
+        private void backupButtonClick(object sender, EventArgs e)
+        {
+           
             string[] files = Directory.GetFiles(save_path);
-            listBox1.Items.Clear();
-            var fileName = Path.Combine((Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)),"Test","Temp","file1.txt");
+        //    listBox1.Items.Clear();
+            var fileName = Path.Combine((Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)), "Test", "Temp", "file1.txt");
             //MessageBox.Show(fileName);
-            File.Copy(fileName, @"C:\temp\copied3.txt" + files.Length);
+            File.Copy(fileName, @"C:\temp\copied.txt"+"_" + files.Length);
+            
+            showFiles();
+        }
+
+        private void DeleteButtonClick(object sender, EventArgs e)
+        {
             foreach (string s in Directory.GetFiles(save_path))
-                listBox1.Items.Add(Path.GetFileName(s)+"     "+File.GetLastWriteTime(s));
+                File.Delete(s);
+            listBox1.Items.Clear();
+            showFiles();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+          //  showFiles();
         }
     }
 }
